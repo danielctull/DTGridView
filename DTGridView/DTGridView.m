c9e613987604955e38ab6ab5bb16136131142b41
Dave Verwer <dave.verwer@shinydevelopment.com>
2010-12-10 16:19:06 +0000

    Fixed typo with numberOfRows in DTGridView.

--- DTGridView/DTGridView.m
+++ DTGridView/DTGridView.m
@@ -770,7 +770,7 @@ NSInteger intSort(id info1, id info2, void *context) {
 #pragma mark Accessors
 
 - (NSInteger)numberOfRows {
-	if (numberOfRows = DTGridViewInvalid) {
+	if (numberOfRows == DTGridViewInvalid) {
 		numberOfRows = [self.dataSource numberOfRowsInGridView:self];
 	}
 	
